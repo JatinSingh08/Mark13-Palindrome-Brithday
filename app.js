2baaf341de58a0a883c596818e2f51e5e0e5b357
JatinSingh08 <singhjatin0812@gmail.com>
2022-09-09 21:12:01 +0530

    update

--- app.js
+++ app.js
@@ -24,4 +24,26 @@ function convertDateToStr(date) {
     dateStr.month = date.month.toString();
    }
    dateStr.year = date.year;
+
+   return dateStr
 }
+
+function getAllDateFormats(date) {
+  var dateStr = convertDateToStr(date);
+
+  var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+  var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+  var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+  var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2); 
+  var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+  var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day.slice(-2);
+
+  return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+}
+
+var date = {
+  day: 2,
+  month: 9,
+  year: 2022
+}
+console.log(getAllDateFormats(date));
